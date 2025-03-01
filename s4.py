4d8f063ad29ac4f6d640fc56a5fd3ab51ad7d5a7
shskkdbdns <yasirmaroof159@gmail.com>
2025-03-01 20:56:06 +0530

    Update s4.py

--- s4.py
+++ s4.py
@@ -6,7 +6,7 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7921815407:AAGUVrf3oHAnyAFmUT6tAgqvh3qmugAxp8s')
+bot = telebot.TeleBot('7921815407:AAEeFegLiPKbQRRcOVMiaSXyJL6LsTYK0C0')
 
 # Admin user IDs
 admin_id = ["1257888659"]
