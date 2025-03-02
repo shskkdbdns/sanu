f4a98b4f68b8a140ccec1a40921cdab339e0207e
shskkdbdns <yasirmaroof159@gmail.com>
2025-03-02 09:57:40 +0530

    Update f.py

--- f.py
+++ f.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7921815407:AAFZHTHo7mppAJp0PP9ITZK93cecwH6VX54')
+bot = telebot.TeleBot('7921815407:AAEHmHz_KZQv_hocPwzZ3ozgRhnaTN45S5g')
 
 # Admin user IDs
 admin_id = ["1257888659"]
